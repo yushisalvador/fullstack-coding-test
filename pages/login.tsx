198782701a63c27879e3058009081d1a92782009
yccjs <yccjsalvador@gmail.com>
2022-10-18 15:30:09 +0900

    feat: implement login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Text, FormControl, Input, FormLabel, Button } from "@chakra-ui/react";
 import { Public } from "auth/authRoute";
+import useAuth from "auth/auth";
 
 function Login() {
+  const auth = useAuth();
+  const { login } = auth;
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
   return (
     <Container mt="20px" w="100vw" h="100vh">
       <Text fontSize="25px">Login to your account. </Text>
       <form>
         <FormControl mt="20px">
           <FormLabel>Email address</FormLabel>
-          <Input type="email" isRequired />
+          <Input type="email" onChange={(e) => setEmail(e.target.value)} isRequired />
           <FormLabel>Password</FormLabel>
-          <Input type="password" isRequired />
+          <Input type="password" onChange={(e) => setPassword(e.target.value)} isRequired />
         </FormControl>
-        <Button type="submit" mt="20px" float="right">
+        <Button
+          type="submit"
+          mt="20px"
+          float="right"
+          onClick={(e) => {
+            e.preventDefault();
+            login(email, password);
+          }}>
           Login{" "}
         </Button>
       </form>
