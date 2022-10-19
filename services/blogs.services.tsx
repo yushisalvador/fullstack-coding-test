7e64f03b56b435702e26f9a0837f0ee0fd56ffb9
yccjs <yccjsalvador@gmail.com>
2022-10-19 17:52:49 +0900

    refactor: use onSnapshot for realtime updates

--- services/blogs.services.tsx
+++ services/blogs.services.tsx
@@ -1,14 +1,8 @@
 import { db } from "auth/config/firebase.config";
-import { AllBlogs } from "types/types";
-import { collection, getDocs, getDoc, addDoc, updateDoc, deleteDoc, doc, QuerySnapshot } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 
 const blogsCollection = collection(db, "blogs");
 
 export const addBlog = (newBlog: object) => {
   return addDoc(blogsCollection, newBlog);
 };
-
-export async function getAllBlogs(): Promise<AllBlogs> {
-  let allblogs = getDocs(blogsCollection) as unknown;
-  return allblogs as AllBlogs;
-}
