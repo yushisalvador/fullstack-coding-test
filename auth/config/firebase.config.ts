73352fd35341d94436c3715ad2cc5cc8c13aedad
yccjs <yccjsalvador@gmail.com>
2022-10-19 12:29:00 +0900

    feat: add storage for media storage

--- auth/config/firebase.config.ts
+++ auth/config/firebase.config.ts
@@ -1,5 +1,7 @@
-import { getAuth } from "firebase/auth";
 import firebase from "firebase/compat/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const app = firebase.initializeApp({
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,3 +13,5 @@ const app = firebase.initializeApp({
 });
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
